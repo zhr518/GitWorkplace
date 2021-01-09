a8d07acbe6e92606e635c6a4adb85ddc2c037f5a
zhanghaoran <2014086284@qq.com>
2021-01-09 22:27:08 +0800

    初始化

---
+++ HelloGit/src/main/java/com/zhang/Hello.java
@@ -0,0 +1,11 @@
+package com.zhang;
+
+/**
+ * @author zhanghaoran
+ * @create 2021-01-09 22:25
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
