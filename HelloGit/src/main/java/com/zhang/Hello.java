c47e6f87fe65103d79b3695ba0e06ca771d43441
zhanghaoran <2014086284@qq.com>
2021-01-10 18:31:55 +0800

    测试SSH

--- HelloGit/src/main/java/com/zhang/Hello.java
+++ HelloGit/src/main/java/com/zhang/Hello.java
@@ -12,6 +12,6 @@ public class Hello {
         System.out.println("主干添加");
         System.out.println("分支添加");
         System.out.println("在线更新");
-
-    }
+        System.out.println("测试SSH");
+     }
 }
